11c01891e9754d646d716cdb5a1d18a99f1774a9
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2025-10-30 16:09:24 +0900

    Complete v1

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
+// common
 const myLibrary = new Array();
+const clickEvent = new Event("click");
 
 function Book(title, author, pages, isRead) {
-  if (!new.target) throw Error("Call constructor with 'new' keyword");
+  if (!new.target) {
+    throw Error("Call constructor with 'new' keyword");
+  }
 
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-  this.readMessage = this.isRead ? "Read" : "Not read";
-
-  this.info = () =>
-    `${this.title} is written by ${this.author}, ${this.pages} pages, ${this.readMessage}`;
 }
+
+Book.prototype.setCard = function () {
+  bookCard.dataset.id = this.id;
+
+  cardTitle.textContent = "Title: " + this.title;
+  cardAuthor.textContent = "Written by " + this.author;
+  cardPages.textContent = this.pages + " Pages";
+  if (this.isRead) {
+    cardReadMessage.textContent = "Status: Read";
+  } else {
+    cardReadMessage.textContent = "Status: Not read";
+  }
+
+  newTitle.value = this.title;
+  newAuthor.value = this.author;
+  newPages.value = this.pages;
+  if (this.isRead) {
+    statusRead.checked = true;
+  } else {
+    statusNotRead.checked = true;
+  }
+};
+
+Book.prototype.update = function () {
+  this.title = newTitle.value;
+  this.author = newAuthor.value;
+  this.pages = Number(newPages.value);
+  this.isRead = statusRead.checked ? true : false;
+};
+
+Book.prototype.remove = function () {
+  const index = myLibrary.indexOf(this);
+  if (index || index === 0) {
+    myLibrary.splice(index, 1);
+  }
+};
+
+Book.prototype.removeIfEmpty = function () {
+  if (!this.title && !this.author && !this.pages) {
+    removeButton.dispatchEvent(clickEvent);
+  }
+};
 
 function addBookToLibrary(title, author, pages, isRead) {
   const book = new Book(title, author, pages, isRead);
@@ -20,15 +62,42 @@ function addBookToLibrary(title, author, pages, isRead) {
   myLibrary.push(book);
 }
 
+// sample
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("Pride and Prejudice", "Jane Austen", 448, true);
 addBookToLibrary("What is History?", "E.H. Carr", 100, false);
 
-const shelfRow = document.querySelector(".shelf-row");
+// sounds
 const bookOpenSound = document.querySelector("#book-open-sound");
 const bookHoverSound = document.querySelector("#book-hover-sound");
-const modalContainer = document.querySelector(".modal-container");
 
+// modal
+const modalContainer = document.querySelector(".modal-container");
+const bookCard = document.querySelector(".book-card");
+
+// modal-view
+const viewForm = document.querySelector(" .view");
+const cardTitle = document.querySelector(" .card-title");
+const cardAuthor = document.querySelector(" .card-author");
+const cardPages = document.querySelector(" .card-pages");
+const cardReadMessage = document.querySelector(" .card-status");
+const editButton = document.querySelector(" .button.edit");
+
+// modal-edit
+const editForm = document.querySelector(" form.edit");
+const newTitle = document.querySelector("#card-title");
+const newAuthor = document.querySelector("#card-author");
+const newPages = document.querySelector("#card-pages");
+const statusRead = document.querySelector("#read");
+const statusNotRead = document.querySelector("#not-read");
+const confirmButton = document.querySelector(" .button.confirm");
+const cancelButton = document.querySelector(" .button.cancel");
+const removeButton = document.querySelector(" .button.remove");
+
+// shelf
+const shelfRow = document.querySelector(".shelf-row");
+
+// shelf-functions
 function displayBooks() {
   myLibrary.forEach((book) => {
     const newBook = document.createElement("div");
@@ -44,15 +113,16 @@ function displayBooks() {
       bookTitle.appendChild(bookTitleWord);
     });
 
-    const bookCard = document.querySelector(".book-card");
-    setCard(book, bookCard);
-
     newBook.addEventListener("click", () => {
+      book.setCard();
       bookCard.showModal();
       bookOpenSound.play();
     });
 
-    newBook.addEventListener("mouseenter", () => bookHoverSound.play());
+    newBook.addEventListener("mouseenter", () =>
+      bookHoverSound.play().catch(() => {})
+    );
+
     newBook.addEventListener("mouseleave", () => {
       bookHoverSound.pause();
       bookHoverSound.currentTime = 0;
@@ -62,60 +132,18 @@ function displayBooks() {
     shelfRow.appendChild(newBook);
   });
 }
-document.querySelector("#card-title").focus();
 
-function setCard(book, bookCard) {
-  // view form
-  const viewForm = document.querySelector(".book-card .view");
-  const cardTitle = document.querySelector(".card-title");
-  const cardAuthor = document.querySelector(".card-author");
-  const cardPages = document.querySelector(".card-pages");
-  const cardReadMessage = document.querySelector(".card-status");
-  const editButton = document.querySelector(".button.edit");
-
-  cardId = book.id;
-  cardTitle.textContent = "Title: " + book.title;
-  cardAuthor.textContent = "Written by " + book.author;
-  cardPages.textContent = book.pages + " Pages";
-  cardReadMessage.textContent = "Status: " + book.readMessage;
-
-  bookCard.addEventListener("close", () => {
-    bookOpenSound.pause();
-    bookOpenSound.currentTime = 0;
-  });
-
-  // edit form
-  const editForm = document.querySelector("form.edit");
-  const newTitle = document.querySelector("#card-title");
-  const newAuthor = document.querySelector("#card-author");
-  const newPages = document.querySelector("#card-pages");
-  const newStatus = document.querySelector("#card-status");
-  const confirmButton = document.querySelector(".button.confirm");
-  const cancelButton = document.querySelector(".button.cancel");
-
-  newTitle.value = book.title;
-  newAuthor.value = book.author;
-  newPages.value = book.pages;
-  newStatus.value = book.isRead;
-
-  editButton.addEventListener("click", () => {
-    viewForm.style.display = "none";
-    editForm.style.display = "contents";
-
-    newTitle.focus();
-  });
-
-  confirmButton.addEventListener("click", () => {
-    viewForm.style.display = "contents";
-    editForm.style.display = "none";
-  });
-
-  cancelButton.addEventListener("click", () => {
-    viewForm.style.display = "contents";
-    editForm.style.display = "none";
-  });
+function refreshShelf() {
+  shelfRow.replaceChildren();
+  displayBooks();
 }
 
+function getBookWithId(bookCard) {
+  const bookId = bookCard.dataset.id;
+  return myLibrary.filter((book) => book.id === bookId)[0];
+}
+
+// mute
 const muteButton = document.querySelector(".button.mute");
 muteButton.addEventListener("click", () => {
   muteButton.classList.toggle("on");
@@ -124,14 +152,66 @@ muteButton.addEventListener("click", () => {
     .forEach((sound) => (sound.muted = !sound.muted));
 });
 
+// add-book
 const addBookButton = document.querySelector(".button.add-book");
-const newBookCard = document.querySelector(".new-book-dialog");
 addBookButton.addEventListener("click", () => {
-  shelfRow.replaceChildren();
   addBookToLibrary("", "", "", "");
-  displayBooks();
-  const clickEvent = new Event("click");
+  refreshShelf();
   shelfRow.lastChild.dispatchEvent(clickEvent);
+  editButton.dispatchEvent(clickEvent);
+});
+
+// close-card
+bookCard.addEventListener("close", () => {
+  bookOpenSound.pause();
+  bookOpenSound.currentTime = 0;
+
+  viewForm.style.display = "contents";
+  editForm.style.display = "none";
+
+  const book = getBookWithId(bookCard);
+  book.removeIfEmpty();
+});
+
+// edit-book
+editButton.addEventListener("click", () => {
+  viewForm.style.display = "none";
+  editForm.style.display = "contents";
+
+  newTitle.focus();
+});
+
+// edit-confirm
+confirmButton.addEventListener("click", () => {
+  const book = getBookWithId(bookCard);
+  book.update();
+  book.removeIfEmpty();
+
+  refreshShelf();
+  viewForm.style.display = "contents";
+  editForm.style.display = "none";
+  bookCard.close();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (editForm.style.display === "contents" && e.key === "Enter") {
+    confirmButton.dispatchEvent(clickEvent);
+  }
+});
+
+// edit-cancel
+cancelButton.addEventListener("click", () => {
+  viewForm.style.display = "contents";
+  editForm.style.display = "none";
+});
+
+// remove
+removeButton.addEventListener("click", () => {
+  const book = getBookWithId(bookCard);
+  book.remove();
+
+  refreshShelf();
+  bookCard.close();
 });
 
 displayBooks();
